def0b9ab1b511ff5328fdae76634fec5e6c775f4
ClydioSeven <angel.magdaleno@cetys.edu.mx>
2024-10-06 14:29:21 -0700

    cedric changes

--- index2.js
+++ index2.js
@@ -23,7 +23,7 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import getStarfield from "./src/getStarfield.js";
 
 const w = window.innerWidth;
-const h = window.innerHeight;
+const h = window.innerHeight-100;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
